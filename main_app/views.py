9c0b6c5636060a1b941ffe56d1691094f65d28dd
Yuki326 <s20a1035@gmail.com>
2021-11-21 13:41:10 +0900

    集計結果画面にとりあえず集計結果とは関係のないグラフを表示させた

--- main_app/views.py
+++ main_app/views.py
@@ -124,3 +124,35 @@ def result(request):
     'data_radio':Answer_radio.objects.all(),
   }
   return render(request,'result.html',params)
+
+import matplotlib
+#バックエンドを指定
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import io
+from django.http import HttpResponse
+
+#グラフ作成
+def setPlt():
+    x = ["07/01", "07/02", "07/03", "07/04", "07/05", "07/06", "07/07"]
+    y = [3, 5, 0, 5, 6, 10, 2]
+    plt.bar(x, y, color='#00d5ff')
+    plt.title(r"$\bf{Running Trend  -2020/07/07}$", color='#3407ba')
+    plt.xlabel("Date")
+    plt.ylabel("km")
+
+# SVG化
+def plt2svg():
+    buf = io.BytesIO()
+    plt.savefig(buf, format='svg', bbox_inches='tight')
+    s = buf.getvalue()
+    buf.close()
+    return s
+
+# 実行するビュー関数
+def get_svg(request):
+    setPlt()  
+    svg = plt2svg()  #SVG化
+    plt.cla()  # グラフをリセット
+    response = HttpResponse(svg, content_type='image/svg+xml')
+    return response
